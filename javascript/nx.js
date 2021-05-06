d683cb50918d1370b3450804de7535238e3aa018
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2021-05-06 21:15:58 +0100

    tic tac toe

---
+++ javascript/nx.js
@@ -0,0 +1,90 @@
+var turn = ("x")
+var x = ("x")
+function aa() {
+	if(turn == x){
+	document.getElementById('aa').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('aa').src='pic/o.png';
+		turn = ("x")
+	}
+}
+function ab() {
+	if(turn == x){
+	document.getElementById('ab').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('ab').src='pic/o.png';
+			turn = ("x")}
+}
+function ac() {
+	if(turn == x){
+	document.getElementById('ac').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('ac').src='pic/o.png';
+			turn = ("x")}
+}
+
+//row 2
+function ba() {
+	if(turn == x){
+	document.getElementById('ba').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('ba').src='pic/o.png';
+		turn = ("x")
+	}
+}
+function bb() {
+	if(turn == x){
+	document.getElementById('bb').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('bb').src='pic/o.png';
+			turn = ("x")}
+}
+function bc() {
+	if(turn == x){
+	document.getElementById('bc').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('bc').src='pic/o.png';
+			turn = ("x")}
+}
+
+//row 3
+function ca() {
+	if(turn == x){
+	document.getElementById('ca').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('ca').src='pic/o.png';
+		turn = ("x")
+	}
+}
+function cb() {
+	if(turn == x){
+	document.getElementById('cb').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('cb').src='pic/o.png';
+			turn = ("x")}
+}
+function cc() {
+	if(turn == x){
+	document.getElementById('cc').src='pic/x.png';
+	turn = ("o")
+	}
+	else{
+	document.getElementById('cc').src='pic/o.png';
+			turn = ("x")}
+}
