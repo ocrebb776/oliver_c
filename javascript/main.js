db64b2764c707b3b2d6dbf177008a586a84d4cb7
ocrebb776 <79861024+ocrebb776@users.noreply.github.com>
2021-05-06 18:08:34 +0100

    find percentage of
    moven menu js to seperate file

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
 }
 
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
-0o
